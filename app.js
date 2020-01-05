c1bf534780822d9866f0da461693d8e70415030d
Luiz Jacó <luizjacomn@gmail.com>
2020-01-05 16:07:18 -0300

    Adicionando métodos CRUD

--- app.js
+++ app.js
@@ -13,12 +13,20 @@ function criarCard() {
         idade: Math.floor(Math.random() * 22 + 18),
         curtidas: 0
     };
-/**
- * .collection: Referenciar uma coleção
- * .doc('documento'): Referenciar um documento
- * .set({dados}): Insere o objeto passado por parâmetro na referência
- */
-    firebase.firestore().collection('cards').doc('1').set(card).then(() => {
+    /**
+     * .collection: Referenciar uma coleção
+     * .doc('documento'): Referenciar um documento
+     * .set({dados}): Insere o objeto passado por parâmetro na referência
+     */
+    // firebase.firestore().collection('cards').doc('1').set(card).then(() => {
+    //     console.log('dados salvos');
+    //     adicionaCardATela(card, 1);
+    // });
+
+    /**
+     * add({dados}): Adiciona os dados dentro de um UID gerado automaticamente
+     */
+    firebase.firestore().collection('cards').add(card).then(() => {
         console.log('dados salvos');
         adicionaCardATela(card, 1);
     });
@@ -29,7 +37,10 @@ function criarCard() {
  * @param {String} id Id do card
  */
 function deletar(id) {
-
+    var card = document.getElementById(id);
+    firebase.firestore().collection('cards').doc(id).delete().then(() => {
+        card.remove();
+    });
 };
 
 /**
@@ -37,7 +48,22 @@ function deletar(id) {
  * @param {String} id Id do card
  */
 function curtir(id) {
+    var card = document.getElementById(id);
+    var count = card.getElementsByClassName('count-number')[0];
+    var button = card.getElementsByClassName('btn-danger')[0];
+    var countNumber = +count.innerText;
+    countNumber++;
 
+    firebase.firestore().collection('cards').doc(id).update({
+        curtidas: countNumber
+    }).then(() => {
+        count.innerText = countNumber;
+        if (countNumber == 0) {
+            button.classList.add('disabled');
+        } else {
+            button.classList.remove('disabled');
+        }
+    });
 };
 
 /**
@@ -45,14 +71,56 @@ function curtir(id) {
  * @param {String} id Id do card
  */
 function descurtir(id) {
-
+    var card = document.getElementById(id);
+    var count = card.getElementsByClassName('count-number')[0];
+    var button = card.getElementsByClassName('btn-danger')[0];
+    var countNumber = +count.innerText;
+    if (countNumber > 0) {
+        countNumber--;
+        firebase.firestore().collection('cards').doc(id).update({
+            curtidas: countNumber
+        }).then(() => {
+            count.innerText = countNumber;
+            if (countNumber == 0) {
+                button.classList.add('disabled');
+            } else {
+                button.classList.remove('disabled');//TODO 11
+            }
+        });
+    }
 };
 
 /**
  * Espera o evento de que a DOM está pronta para executar algo
  */
 document.addEventListener("DOMContentLoaded", function () {
+    /**
+     * get(): Busca os documentos apenas no carregamento
+     */
+    // firebase.firestore().collection('cards').get().then((snapshot) => {
+    //     snapshot.docs.forEach(doc => {
+    //         adicionaCardATela(doc.data(), doc.id);
+    //     });
+    // });
 
+    /**
+     * onSnapshot(): responde apenas às mudanças dos documentos afetados
+     */
+    firebase.firestore().collection('cards').onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+            switch (change.type) {
+                case 'added':
+                    adicionaCardATela(change.doc.data(), change.doc.id);
+                    break;
+                case 'modified':
+                    console.log('modified');
+                    break;
+                case 'removed':
+                    console.log('removed');
+                    break;
+            }
+        });
+    });
 });
 
 /**
@@ -84,7 +152,7 @@ function adicionaCardATela(informacao, id) {
     inner.classList.add('row')
     // Botão adicionar
     let button_add = document.createElement("button");
-    button_add.classList.add('btn', 'btn-link', 'col-3');
+    button_add.classList.add('btn', 'btn-success', 'btn-link', 'col-2');
     button_add.setAttribute('onclick', "curtir('" + id + "')");
     button_add.innerText = '+';
     inner.appendChild(button_add);
@@ -97,11 +165,16 @@ function adicionaCardATela(informacao, id) {
 
     // Botão de subtrair
     let button_sub = document.createElement("button");
-    button_sub.classList.add('btn', 'btn-link', 'col-3');
+    button_sub.classList.add('btn', 'btn-danger', 'btn-link', 'col-2');
     button_sub.setAttribute('onclick', "descurtir('" + id + "')");
     button_sub.innerText = '-';
     inner.appendChild(button_sub);
     // ===================================
+
+    // Espaço
+    let spacer = document.createElement("div");
+    spacer.classList.add('col-1');
+    inner.appendChild(spacer);
 
     // Botão de excluir
     let button_del = document.createElement("button");
@@ -119,6 +192,7 @@ function adicionaCardATela(informacao, id) {
     card.id = id;
     let card_body = document.createElement("div");
     card_body.classList.add('card-body');
+    card.style.marginBottom = '10px';
     // ===================================
 
     // popula card
